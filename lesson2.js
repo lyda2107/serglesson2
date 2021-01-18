7bdd72106a990f16fb537415e007b91136ff2e8c
lyda2107 <kacherovskayyluda@gmai.com>
2021-01-19 01:42:17 +0200

    Update lesson2.js

--- lesson2.js
+++ lesson2.js
@@ -46,11 +46,35 @@ let i = 0
  }
 */
 //- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині
-
+/*
 let i = 0
  while(i < 20) {
     document.write('<h1> Fava: '+i+' </h1>')
   i++   
  }
-
+*/
 //Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+ /*
+const masuv=[45, 42, 13, 46, 78, 89, 56, 14, 36, 72]
+ for (let i = 0; i < masuv.length; i++) {
+     console.log (masuv[i]);
+     
+ }
+*/
+ //- Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
+ /*
+ const masuv1=['work', 'house', 'child', 'parens', 'brouser','work', 'house', 'child', 'parens', 'brouser']
+ for (let i = 0; i < masuv1.length; i++) {
+     console.log (masuv1[i]);
+ }
+ */
+//- Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+/*
+const masuv1=['work', 'house', 'child', 'parens', 8===8, 45, 42, 13, 46, 8<=7]
+for (let i = 0; i < masuv1.length; i++) {
+    console.log (masuv1[i]);
+}
+*/
+//- Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+const masuv1=['work', 'house', 'child', 'parens', 8===8, 45, 42, 13, 46, 8<=7]
+console.count(typeof Boolean)
